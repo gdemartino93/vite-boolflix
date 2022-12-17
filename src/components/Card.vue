a00d8318f5fa980174f199a74296648b6651cc4f
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2022-12-17 23:01:28 +0100

    i ve a delay on mouseover and mouseleave

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -4,20 +4,25 @@ export default{
 props :["item"],
 data(){
   return{
-    store
+    store,
+    cardVisible : true,
+    
   }
 }
 }
 </script>
 
 <template>
-    <div class="item">
+
+    <div class="item" v-show="cardVisible==true" @mouseover="cardVisible = false" @mouseleave="cardVisible = true">
       <img :src="store.imgEndPoint + item.poster_path" :alt="item.title">
+
+    </div>
+    <div class="item-back" v-show="cardVisible==false">
       <span class="title">{{ item.title }}</span>
       <span class="title-original">{{ item.original_title }}</span>
       <span class="leng">{{ item.original_language }}</span>
       <span class="vote"> {{ item.vote_average }} </span>
-     
     </div>
 </template>
 
@@ -30,7 +35,14 @@ data(){
     background: bisque;
     display: flex;
     flex-direction: column;
+}
+.item-back{
+  width: 200px;
 
+border: 1px solid black;
+background: red;
+display: flex;
+flex-direction: column;
 }
 
 </style>
