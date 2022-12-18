c611e45410749422ac8dba90d449422232d4c419
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2022-12-18 20:42:17 +0100

    import font awesome

--- src/main.js
+++ src/main.js
@@ -5,5 +5,18 @@ import "bootstrap/dist/css/bootstrap.css"
 import axios, {isCancel, AxiosError} from 'axios';
 
 import 'animate.css';
+import 'font-awesome/css/font-awesome.css'
+
+// /* import the fontawesome core */
+// import { library } from '@fortawesome/fontawesome-svg-core'
+
+// /* import font awesome icon component */
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// /* import specific icons */
+// import { faStar } from '@fortawesome/free-solid-svg-icons'
+
+// /* add icons to the library */
+// library.add(faStar)
 
 createApp(App).mount('#app')
